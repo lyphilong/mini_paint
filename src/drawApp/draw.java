ea2b712b352b709fed889b97a4e542c4acbfea98
KhanhPhan <pqkhanh561@gmail.com>
2019-12-05 13:24:11 +0700

    create a simple frame

--- src/drawApp/draw.java
+++ src/drawApp/draw.java
@@ -4,9 +4,13 @@ import java.awt.event.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.awt.geom.*;
 
 public class draw {
 	private JFrame mainFrame;
+	private draw.DrawPanel dp = new DrawPanel();
+	//private draw.ControlPanel cp = new ControlPanel(dp);	
 	//Contructor
 	public draw(){
 		Gui();
@@ -16,13 +20,17 @@ public class draw {
 	//The pop up menu when cliked right button of mouse
 	class PopUpDemo extends JPopupMenu {
 	    //TODO: Need add spec in here
-	    JMenuItem anItem;
+	    JMenuItem color;
+	    JMenuItem delete;
 	    public PopUpDemo() {
-		anItem = new JMenuItem("Click Me!");
-		add(anItem);
+		color = new JMenuItem("Color");
+		delete = new JMenuItem("Delete");
+		add(color);
+		add(delete);
 	    }
 	}
 
+	//TODO: Add more action in th PopUp
 	class PopClickListener extends MouseAdapter {
 	    public void mousePressed(MouseEvent e) {
 		if (e.isPopupTrigger())
@@ -42,50 +50,77 @@ public class draw {
 
 	public static void main(String[] args) {
 		draw d = new draw();
-		System.out.println("hello");
 		
 	    }
 	private void Gui(){
 		mainFrame = new JFrame("Frame");	
-		mainFrame.setSize(600,500);
+		mainFrame.setSize(1600,1500);
 		mainFrame.setLayout(new BorderLayout());
 		mainFrame.addMouseListener(new PopClickListener());
+		//TODO: Set to 2 class
 		mainFrame.add(new ControlPanel(), BorderLayout.WEST);
+		dp.setSize(1500,1500);
+		dp.setLayout(new GridLayout());
+		dp.setVisible(true);
+		dp.setBackground(Color.GREEN);
+		mainFrame.add(dp);
  	    	mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setVisible(true);
+//		mainFrame.pack();
 	}
 
-	private void createPanel(int x){
-		JPanel panel = new JPanel();
-		mainFrame.add(panel);
-		JButton button = new JButton("button 1");
-		JButton button1 = new JButton("button 2");
-		button.addActionListener(new ActionListener(){
-			@Override
-            		public void actionPerformed(ActionEvent arg0) {
-				System.out.println("hello");
-			}
-		});
-		panel.add(button);
-		panel.add(button1);
-
-	}
 	public class ControlPanel extends JPanel{
 		//TODO: Control which will draw
 		public ControlPanel(){
 		this.setLayout(new BorderLayout());
 		JPanel p = new JPanel();
+		//TODO: Need the same size on button
 		JButton undo = new JButton("undo");
 		JButton clear= new JButton("clear");
 
 		//Set size of button
-		undo.setPreferredSize(new Dimension(140,140));	
-		clear.setPreferredSize(new Dimension(140,140));	
+		undo.setSize(140,140);	
+		clear.setSize(140,140);	
 		
 		p.add(undo);
 		p.add(clear);
 		p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
 		this.add(p, BorderLayout.CENTER);
+		}
+	}
+
+
+	public class Rect{
+		private float width;
+		private float height;	
+
+		public Rect(){
+			width = 150;
+			height = 150;
+		}
+
+		public draw(Graphics2D g){
+			g2d.drawRect(150,150,150,150);
+		}
+	}
+
+
+	public class DrawPanel extends JPanel{
+		public DrawPanel(){
+		}
+		@Override
+		public Dimension getPreferredSize(){
+			return new Dimension(500,500);
+		}
+		
+		//TODO: Add shape to draw n here
+		@Override 
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			Graphics2D g2d = (Graphics2D) g.create();
+
+			//g2d.drawRect(150,150,150,150);
+			g2d.dispose();
 		}
 	}
 }
